5f90c613b119c5abeb330105d295063ed663b11c
Topejay <121813650+Topejay@users.noreply.github.com>
2023-01-05 12:30:47 +0100

    Add files via upload

---
+++ scripter.js
@@ -0,0 +1,46 @@
+let tope = 26;
+let tayo = 27;
+let namey = "tope1";
+let surnamejh = "Awogbemila";
+
+console.log(tope, tayo, namey, surnamejh);
+
+let gg = true;
+console.log(typeof gg, gg, typeof tope, typeof namey);
+
+let fula;
+console.log(fula);
+
+let n = tope + tayo;
+console.log(n);
+
+let myname = "Ayo";
+let surname = "awogbemils";
+let yearbirth = 1993;
+let thisyerere = 2023;
+
+console.log("My name is Ayo, i am 29 years old");
+
+console.log(
+  "my" +
+    " " +
+    "name" +
+    " " +
+    "is" +
+    " " +
+    "Ayo," +
+    " " +
+    "I" +
+    " " +
+    "am" +
+    " " +
+    (thisyerere - yearbirth) +
+    " " +
+    "years" +
+    " " +
+    "old"
+);
+
+let mily = `My name is ${myname} i am ${thisyerere - yearbirth} years old`;
+
+console.log(mily);
